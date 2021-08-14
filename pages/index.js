c61986cd405293d9ecd258bedd621180818630c3
kawabesan <74655770+kawabesan@users.noreply.github.com>
2021-08-14 15:27:48 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             趣味：　読書、ゲーム、サイクリング（愛車を手放したため現在はできてない）
             <br/><br/>
 
-            ウェブ制作に興味を持ち、独学でHTML・CSS、Photshopを学習。<br/>
+            ウェブ制作に興味を持ち、独学でHTML・CSS、Photoshopを学習。<br/>
             その後、プログラミングにも興味を持ち、スクールにて3ヶ月間で<br/>
             JavaScript・Reactなどの学習を行う
             <br />
